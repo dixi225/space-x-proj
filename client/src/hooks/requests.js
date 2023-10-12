a6cf1741b2b383efaf0628b5cbbc4f9431a3f286
Harsh Dixit <harshdixit573@gmail.com>
2023-10-13 00:24:53 +0530

    api added

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,14 +1,17 @@
 async function httpGetPlanets() {
   const response=await fetch('http://localhost:8000/planets')
-  return await response.json()
-  
+  const data=await response.json()
+  return data?.habitablePlanets
 }
 
-async function httpGetLaunches() {
-  // TODO: Once API is ready.
   // Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+  const response=await fetch('http://localhost:8000/launches')
+  const data=await response.json()
+  return data.sort((a,b)=>{
+    return a.flight_number-b.flight_number
+  })
 }
-
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
   // Submit given launch data to launch system.
